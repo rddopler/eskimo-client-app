3a1d0abcc6ad192813df0238c50280d14e905af8
Ryan Dopler <rddopler@gmail.com>
2022-06-20 08:08:41 -0400

    Configure Routing Module For 404 Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {MenuComponent} from "./components/menu/menu.component";
 import {ReservationComponent} from "./components/reservation/reservation.component";
 import {PrivateDiningComponent} from "./components/private-dining/private-dining.component";
 import {ContactComponent} from "./components/contact/contact.component";
+import {PageNotFoundComponent} from "./components/common/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'menus', component: MenuComponent },
   { path: 'reservation', component: ReservationComponent },
   { path: 'private-dining', component: PrivateDiningComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
